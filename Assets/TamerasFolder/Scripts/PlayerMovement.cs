46dc4885968be30aa231a5b8d136a20f811eed95
TameraDaniel <2424818@students.wits.ac.za>
2023-09-26 11:27:25 +0200

    1st type of shooting complete

--- Assets/TamerasFolder/Scripts/PlayerMovement.cs
+++ Assets/TamerasFolder/Scripts/PlayerMovement.cs
@@ -7,9 +7,11 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 5f;
 
     public Rigidbody2D rb;
+    public Camera cam;
     //public Animator animator;
 
     Vector2 movement;
+    Vector2 mousePos;
 
     // Update is called once per frame
     void Update()
@@ -20,10 +22,18 @@ public class PlayerMovement : MonoBehaviour
         //animator.SetFloat("Horizonal", movement.x);
         //animator.SetFloat("Vertical", movement.y);
         //animator.SetFloat("Speed", movement.sqrMagnitude);
+
+       mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
     }
 
     private void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+        Vector2 lookDir = mousePos - rb.position;
+        // function that returns the angle between the x axis and 2d vector starting at 0 and terminating at x,y
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
+        
     }
 }
